eb6066ddc6ffdc133742de94e2c5ad02576a0d27
joadguer <josueadrianib@gmail.com>
2025-09-03 18:44:28 -0500

    rm unused

--- db/init.sql
+++ db/init.sql
@@ -48,38 +48,6 @@ CREATE INDEX idx_habits_user      ON habits(user_id);
 CREATE INDEX idx_logs_habit_day   ON logs(habit_id, day);
 
 
--- Publicaciones que hacen los usuarios
--- CREATE TABLE IF NOT EXISTS posts (
---   id INT AUTO_INCREMENT PRIMARY KEY,
---   user_id INT NOT NULL,
---   content TEXT NOT NULL,
---   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
---   FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
--- ) ENGINE=InnoDB;
-
--- -- Comentarios en publicaciones
--- CREATE TABLE IF NOT EXISTS comments (
---   id INT AUTO_INCREMENT PRIMARY KEY,
---   post_id INT NOT NULL,
---   user_id INT NOT NULL,
---   content TEXT NOT NULL,
---   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
---   FOREIGN KEY (post_id) REFERENCES posts(id) ON DELETE CASCADE,
---   FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
--- ) ENGINE=InnoDB;
-
--- -- Reacciones (like, clap, etc.)
--- CREATE TABLE IF NOT EXISTS reactions (
---   id INT AUTO_INCREMENT PRIMARY KEY,
---   post_id INT NOT NULL,
---   user_id INT NOT NULL,
---   type ENUM('like','clap','star') NOT NULL,
---   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
---   UNIQUE(post_id, user_id, type), -- cada user solo 1 reacción de un tipo por post
---   FOREIGN KEY (post_id) REFERENCES posts(id) ON DELETE CASCADE,
---   FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
--- ) ENGINE=InnoDB;
-
 -- Posts
 CREATE TABLE IF NOT EXISTS posts (
   id          INT AUTO_INCREMENT PRIMARY KEY,
